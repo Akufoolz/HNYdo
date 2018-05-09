49aec080609d836623810c65ddc27682b03492d4
Akufoolz <akufoolz@gmail.com>
2018-05-09 01:04:07 -0600

    added inital vuejs

---
+++ app.js
@@ -0,0 +1,10 @@
+let activeList = new Vue({
+    el: '#activeList',
+    data: {
+       todoItems: [
+           {text: "Hello, World!"},
+           {text: "Punch the baby's face"},
+           {text: "Kiss the Honey Bunny"}
+       ]
+    }
+})
